5917d7cc4574d12c298fc38f7c5944565311373a
Kim-WonJoon <skygood96@gmail.com>
2025-03-27 20:33:36 +0900

    csv -> DB 100개씩 개선코드

--- src/main/java/com/example/team5project/domain/csvtodb/DatabaseUtil.java
+++ src/main/java/com/example/team5project/domain/csvtodb/DatabaseUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseUtil {
 
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/shopping_mall";
     private static final String USER = "root";
     private static final String PASSWORD = "1478";
 
